c1c7521e4cd62713c8f3580cd6f965d1c093a229
Max <max_address@yahoo.com>
2016-11-20 15:38:03 +0100

    Horse Run

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # phaserIOGame
 Playing with Phaser.io  
-Doin' a game for my daughter with [phaser.io](https://phaser.io/) and [OpenGameArt](http://opengameart.org/) assets ;)
+Doin' a game for my daughter with [phaser.io](https://phaser.io/) and [Horse of Spring](http://opengameart.org/content/2d-platformer-art-assets-from-horse-of-spring) OpenGameArt assets ;)
--- game.js
+++ game.js
@@ -1,32 +1,38 @@
+'use strict';
+
 var game = new Phaser.Game(2048, 512, Phaser.CANVAS, 'phaser-example',
     { preload: preload, create: create, update: update });
 
 /**
- * Initialisation / chargement des assets
+ * Init / loading assets
  */
 function preload() {
-    // Mise à l'échelle
-    game.scale.maxWidth = 2048;
-    game.scale.maxHeight = 512;
-    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.game.scale.maxWidth = 2048;
+    this.game.scale.maxHeight = 512;
+    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
     // chargement des resources 
-    game.load.image('background', 'assets/backgrounds/scroll_bg_far.png');
-    game.load.image('hills', 'assets/backgrounds/hills-scroll.png');
+    this.game.load.image('background', 'assets/backgrounds/scroll_bg_far.png');
+    this.game.load.image('hills', 'assets/backgrounds/hills-scroll.png');
+
+    this.game.load.atlas('horse-run', 'assets/horse-run.png', 'assets/horse-run.json');
 }
 
 /**
- * Création des éléments sur la zone de jeu
+ * Adding elements to the game
  */
 function create() {
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
     this.back = this.game.add.tileSprite(0, 0, 2048, 512, 'background');
     this.hills = this.game.add.tileSprite(0, 256, 2048, 256, 'hills');
+
+    this.horse = this.game.add.sprite(75, 380, 'horse-run', 'horse');
+    this.horse.animations.add('run', Phaser.Animation.generateFrameNames('horse-run-', 0, 6, '', 2), 10, true);
 }
 
 /**
- * Boucle principale (requestAnnimationFrame)
+ * main loop
  */
 function update() {
     if (this.cursors.left.isDown) 
@@ -34,11 +40,17 @@ function update() {
         //scroll the tile sprites by an amount of pixels on the X axis
         this.back.tilePosition.x += 2;
         this.hills.tilePosition.x += 5;
+ 
     } 
     else if (this.cursors.right.isDown) 
     {
         //scroll the tile sprites by an amount of pixels on the X axis
         this.back.tilePosition.x -= 2;
         this.hills.tilePosition.x -= 5;
-    } 
+        this.horse.animations.play('run');
+    }
+    else
+    {
+        this.horse.animations.stop('run');
+    }
 }
